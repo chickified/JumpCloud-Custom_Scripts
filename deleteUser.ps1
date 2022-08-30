19ed9041c5a3947366f8535edd9eb917001c1fe3
Nestor Lee <hello@nestorlee.com>
2022-08-30 23:16:00 +0900

    Add files via upload

---
+++ deleteUser.ps1
@@ -0,0 +1,9 @@
+﻿$enumerateUsers = Get-LocalUser
+$countOfUsers = Get-LocalUser | measure
+$indexOfDisabledUsers = @()
+
+For($i=0; $i -lt $countOfUsers.Count; $i++){
+    if($enumerateUsers[$i].Description -eq "Added by JumpCloud" -and $enumerateUsers[$i].Enabled -eq $false){
+        Remove-LocalUser -Name $enumerateUsers[$i].Name
+    }
+}
