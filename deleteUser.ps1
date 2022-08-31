1384830c4b30b8d400f24788cd1fb09585f2f031
Nestor Lee <hello@nestorlee.com>
2022-08-31 19:43:59 +0900

    Update deleteUser.ps1

--- deleteUser.ps1
+++ deleteUser.ps1
@@ -1,9 +1,12 @@
-﻿$enumerateUsers = Get-LocalUser
-$countOfUsers = Get-LocalUser | measure
-$indexOfDisabledUsers = @()
+$script={
+    $enumerateUsers = Get-LocalUser
+    $countOfUsers = Get-LocalUser | measure
 
-For($i=0; $i -lt $countOfUsers.Count; $i++){
-    if($enumerateUsers[$i].Description -eq "Added by JumpCloud" -and $enumerateUsers[$i].Enabled -eq $false){
-        Remove-LocalUser -Name $enumerateUsers[$i].Name
+    For($i=0; $i -lt $countOfUsers.Count; $i++){
+        if($enumerateUsers[$i].Description -eq "Added by JumpCloud" -and $enumerateUsers[$i].Enabled -eq $false){
+            Remove-LocalUser -Name $enumerateUsers[$i].Name
+        }
     }
 }
+
+$installed64 = @(& "$env:SystemRoot\sysnative\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy Bypass -NoProfile -NonInteractive -Command $script)
