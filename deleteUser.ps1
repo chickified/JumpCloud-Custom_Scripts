67ae38a3c4cdb20ad04e695c2c920fe5e471483f
Nestor Lee <hello@nestorlee.com>
2022-08-31 19:44:16 +0900

    Update deleteUser.ps1

--- deleteUser.ps1
+++ deleteUser.ps1
@@ -1,6 +1,6 @@
 $script={
     $enumerateUsers = Get-LocalUser
-    $countOfUsers = Get-LocalUser | measure
+    $countOfUsers = $enumerateUsers | measure
 
     For($i=0; $i -lt $countOfUsers.Count; $i++){
         if($enumerateUsers[$i].Description -eq "Added by JumpCloud" -and $enumerateUsers[$i].Enabled -eq $false){
